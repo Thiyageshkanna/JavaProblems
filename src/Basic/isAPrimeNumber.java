c63bb031f3eaed9714eed9a8dcd3b6496a465836
thiyagesh kanna <thiyagesh14092000@gmail.com>
2021-07-26 11:31:18 +0530

    StarPatternDecreasing

--- src/Basic/isAPrimeNumber.java
+++ src/Basic/isAPrimeNumber.java
@@ -6,22 +6,24 @@ import java.util.Scanner;
 public class isAPrimeNumber {
     public static void main(String[] args) {
         Scanner scn=new Scanner(System.in);
-        int totalCount= scn.nextInt();
-        for(int i=0;i<totalCount;i++){
+        int total = scn.nextInt();
+        for(int i=0;i<total;i++){
             int userInput= scn.nextInt();
-          int count =0;
-          for(int div=2;div*div<=userInput;div++){
-              if(userInput%div==0){
-                  count++;
-                  break;
-              }
-          }
-          if(count==0){
-              System.out.println("prime number");
-          }
-          else{
-              System.out.println("not prime");
-          }
+            int count =0;
+            for(int j=2;j<=userInput/2;j++){
+                if(userInput%j==0){
+                    count++;
+                    break;
+                }
+            }
+            if(count==0){
+                System.out.println("prime");
+            }
+            else {
+                System.out.println("not prime");
+            }
+
         }
+
     }
 }
