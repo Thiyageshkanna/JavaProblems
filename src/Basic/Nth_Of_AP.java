eae35621e1bf05d05ccae315674c3582d980ef26
thiyagesh kanna <thiyagesh14092000@gmail.com>
2021-08-01 13:07:33 +0530

    Date:01/08/2021

---
+++ src/Basic/Nth_Of_AP.java
@@ -0,0 +1,27 @@
+package Basic;
+
+import java.util.Scanner;
+
+public class Nth_Of_AP {
+    public static void main(String[] args) {
+        Scanner scn =new Scanner(System.in);
+        System.out.println("Enter Difference");
+        int d=scn.nextInt();  //Difference
+        System.out.println("Enter Starting Number");
+        int a= scn.nextInt(); //Starting Number
+        System.out.println("Enter Final Number");
+        int n= scn.nextInt(); //Final Number
+
+        int result=a+((n-1)*d); //Formula
+
+        System.out.println("The nth element is "+result);
+
+//        To print the list of number with given Input
+        System.out.print(a);
+        for(int i=1;i<n;i++){
+            int finalResult= a+d;
+            System.out.print(finalResult+" ");
+            a=finalResult;
+        }
+    }
+}
