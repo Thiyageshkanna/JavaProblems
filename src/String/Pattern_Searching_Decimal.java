665f06794c6fea147783af277b4b670d11e7dc61
thiyagesh kanna <thiyagesh14092000@gmail.com>
2021-08-17 01:59:26 +0530

    Date:17/08/2021

---
+++ src/String/Pattern_Searching_Decimal.java
@@ -0,0 +1,23 @@
+package String;
+
+import java.util.Scanner;
+
+//Input:12.234;
+//output:234; means After Decimal Points
+public class Pattern_Searching_Decimal {
+    public static void main(String[] args) {
+        Scanner scn=new Scanner(System.in);
+        System.out.println("Enter the Decimal Number: ");
+        double input=scn.nextDouble();
+        String convert=Double.toString(input);
+        int position=convert.indexOf(".");
+        if(position>=0){
+            position+=1;
+            while(position<=convert.length()-1){
+                System.out.print(convert.charAt(position)+" ");
+                position++;
+            }
+
+        }
+    }
+}
